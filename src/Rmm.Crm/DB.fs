c923c906c582a35d7d2a17d38f65a4c6f6ffb9ed
Brian Kjöller Vergmann <brve@demant.com>
2022-08-07 13:47:55 +0200

    DB as object

--- src/Rmm.Crm/DB.fs
+++ src/Rmm.Crm/DB.fs
@@ -3,30 +3,52 @@ namespace Rmm
 open Microsoft.Data.SqlClient
 open SqlHydra.Query
 
-type DB = DB of string
+type DB(connectionString: string) =
+    let getConnection = new SqlConnection(connectionString)
 
-module DB =
-    let connStr input = input |> fun (DB str) -> str
-
-    // let connectionString =
-    //     // "Server=KBNDVCRM80\\DEV01; Database=RMM_MSCRM; Integrated Security=SSPI; Connect Timeout=3; TrustServerCertificate=True"
-    //     // "Server=KBNDVSQL110\\DEV01; Database=RMM_MSCRM; Integrated Security=SSPI; Connect Timeout=3; TrustServerCertificate=True"
-    //     "Server=KBNTSSQL38\\TEST02; Database=RMM_MSCRM; Integrated Security=SSPI; Connect Timeout=3; TrustServerCertificate=True"
-
-    let getConnection db = new SqlConnection(connStr db)
-
-    let openConnection db =
-        let conn = getConnection db
+    let openConnection =
+        let conn = getConnection 
         conn.Open()
         conn
 
     let compiler =
         SqlKata.Compilers.SqlServerCompiler()
 
-    let getContext db =
-        let conn = openConnection db
+    let getContext =
+        let conn = openConnection
         new QueryContext(conn, compiler)
 
-    let toSql (query: SqlKata.Query) =
+    member this.toSql(query: SqlKata.Query) =
         // let compiler = SqlKata.Compilers.SqlServerCompiler()
         compiler.Compile(query).Sql
+
+    member this.ConnectionString = connectionString
+    member this.GetContext() = getContext
+
+
+
+// module DB =
+// let connStr input = input |> fun (DB str) -> str
+
+// let connectionString =
+//     // "Server=KBNDVCRM80\\DEV01; Database=RMM_MSCRM; Integrated Security=SSPI; Connect Timeout=3; TrustServerCertificate=True"
+//     // "Server=KBNDVSQL110\\DEV01; Database=RMM_MSCRM; Integrated Security=SSPI; Connect Timeout=3; TrustServerCertificate=True"
+//     "Server=KBNTSSQL38\\TEST02; Database=RMM_MSCRM; Integrated Security=SSPI; Connect Timeout=3; TrustServerCertificate=True"
+
+// let getConnection (db:DB) = new SqlConnection(db.ConnectionString)
+//
+// let openConnection db =
+//     let conn = getConnection db
+//     conn.Open()
+//     conn
+//
+// let compiler =
+//     SqlKata.Compilers.SqlServerCompiler()
+//
+// let getContext db =
+//     let conn = openConnection db
+//     new QueryContext(conn, compiler)
+//
+// let toSql (query: SqlKata.Query) =
+//     // let compiler = SqlKata.Compilers.SqlServerCompiler()
+//     compiler.Compile(query).Sql
--- src/Rmm.Crm/Data.fs
+++ src/Rmm.Crm/Data.fs
@@ -4,23 +4,20 @@ open SqlHydra.Query
 open Rmm.Crm
 
 module Data =
-    let context db = DB.getContext db
 
     module Logs =
         let systemLogEntryTable =
             table<dbo.dgs_systemlogentryBase>
             |> inSchema (nameof dbo)
 
-        let count db =
-            let ctx = context db
-            selectAsync HydraReader.Read (Shared ctx) {
+        let count (db:DB) =
+            selectAsync HydraReader.Read (Create db.GetContext) {
                 for log in systemLogEntryTable do
                     count
             }
 
-        let latest db =
-            let ctx = context db
-            selectAsync HydraReader.Read (Shared ctx) {
+        let latest (db:DB) =
+            selectAsync HydraReader.Read (Create db.GetContext) {
                 for log in systemLogEntryTable do
                     take 5
                     toArray
